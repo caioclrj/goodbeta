cebbda46ab93140c13358627c750bc6d0d615254
leonardoboav <leonardo0707@gmail.com>
2025-03-28 20:11:07 -0300

    activity & layout de feedback

---
+++ app/src/main/java/com/goodbeta/cliente/activity/FeedbackActivity.kt
@@ -0,0 +1,12 @@
+package com.goodbeta.cliente.activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.goodbeta.cliente.R
+
+class FeedbackActivity : AppCompatActivity(R.layout.feedback_activity) {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+}
