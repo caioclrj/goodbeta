96b83afde30b1f0a3de42b1e06692413774ae901
Tinicco <bernardojos70@yahoo.com>
2025-03-28 23:14:19 -0300

    Commit login_activity e arquivos para styles.

--- app/src/main/java/com/goodbeta/cliente/MainActivity.kt
+++ app/src/main/java/com/goodbeta/cliente/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GoodBetaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Aparecidas",
+                        name = "Bernardo",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
